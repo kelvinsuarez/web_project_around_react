152abaca1df2576296e808084ab7e403089070e5
Kelvin Suarez <k_suarez22@hotmail.com>
2024-04-26 11:21:01 -0700

    creacion de componente PopupWithForm y sustitucion de elementos por instancias de componente

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
 
 
 function App() {
@@ -15,35 +16,25 @@ function App() {
 
         <Footer/>
 
-        <div className="popup-confirmation popup-confirmation-opened">
-          <div className="popup-confirmation__group">
-            <span className="popup-confirmation__icon-close popup-confirmation__icon-close:hover">+</span>
-            <div className="popup-confirmation__container">
-              <h2 className="popup-confirmation__text">¿Estas seguro/a?</h2>
-              <button className="popup-confirmation__button-delete">si</button>
-            </div>
-          </div>
-        </div>
+        <PopupWithForm name="-confirmation" id="" title="¿Estas seguro/a?">
+          <button className="popup-confirmation__button-delete">si</button>
+        </PopupWithForm>
 
-        <div className="popup-image-profile popup-opened" id="popup-image-profile_container" >
-          <div className="popup-image-profile__group">
-            <h2 className="popup-image-profile__icon-close popup-image-profile__icon-close:hover">+</h2>
-            <form name="change_image" className="popup-image-profile__container form-popup" noValidate>
-              <h2 className="popup-image-profile__text" >Cambiar foto de perfil</h2>
-              <input type="url"
-                id="url-profile"
-                placeholder="Imagen URL"
-                minLength="2" maxLength="200"
-                value=""
-                className="popup-image-profile__imput-text form-imput-text"
-                required
-                autoComplete="off"
-              />
-              <span className="url-profile-error form-input-show-error"></span>
-              <button className="popup-save popup-image-profile__button-save popup-image-profile__button-save:hover">Guardar</button>
-            </form>
-          </div>
-        </div>
+        <PopupWithForm name="-image-profile" id="popup-image-profile_container" title="Cambiar foto de perfil">
+          <input 
+            type="url"
+            id="url-profile"
+            placeholder="Imagen URL"
+            minLength="2" maxLength="200"
+            value=""
+            className="popup-image-profile__imput-text form-imput-text"
+            required
+            autoComplete="off"
+          />
+          <span className="url-profile-error form-input-show-error"></span>
+          <button className="popup-save popup-image-profile__button-save popup-image-profile__button-save:hover">Guardar</button>
+        </PopupWithForm>
+
       
         <div className="image-zoom image-zoom_opened" id="image-zoom_container" >
           <div className="image-zoom__group">
@@ -53,68 +44,59 @@ function App() {
           </div>
         </div>
 
-        <div className="popup popup-opened form" id="popup_container" >
-          <div className="popup__group">
-            <h2 className="popup__icon-close popup__icon-close:hover">+</h2>
-            <form name="profile" className="popup__container form__popup" id="form" noValidate>
-              <h2 className="popup__text" >Editar Perfil</h2>
-              <input type="text" 
-                id="nombre"
-                name="nombre"
-                placeHolder="Nombre" 
-                value="Jacques Cousteau" 
-                minLength="2" maxlength="40" 
-                className="popup__imput-text popup__imput-text_name form__imput-text"
-                required
-                autoComplete="off"
-              />
-              <span className="nombre-error form__input-show-error"></span>
-              <input type="text" 
-                id="acerca"
-                name="acerca"
-                placeholder="A cerca de mi"
-                value="Explorador"
-                minlength="2" maxlength="200"
-                className="popup__imput-text popup__imput-text_job form__imput-text"
-                required
-                autoComplete="off"
-              />
-              <span className="acerca-error form__input-show-error"></span>
-              <button className="popup-save popup__button-save popup__button-save:hover" id="button-save">Guardar</button>
-            </form>
-          </div>
-        </div>
+        <PopupWithForm name="-profile" id="popup-profile_container" title="Editar Perfil">
+          <input 
+            type="text" 
+            id="nombre"
+            name="nombre"
+            placeHolder="Nombre" 
+            value="Jacques Cousteau" 
+            minLength="2" maxlength="40" 
+            className="popup-profile__imput-text popup-profile__imput-text_name form__imput-text"
+            required
+            autoComplete="off"
+          />
+          <span className="nombre-error form__input-show-error"></span>
+          <input 
+            type="text" 
+            id="acerca"
+            name="acerca"
+            placeholder="A cerca de mi"
+            value="Explorador"
+            minlength="2" maxlength="200"
+            className="popup-profile__imput-text popup-profile__imput-text_job form__imput-text"
+            required
+            autoComplete="off"
+          />
+          <span className="acerca-error form__input-show-error"></span>
+          <button className="popup-save popup-profile__button-save popup-profile__button-save:hover" id="button-save">Guardar</button>
+        </PopupWithForm>
 
-        <div className="popup-place popup-opened form" id="popup-place_container" >
-          <div className="popup-place__group">
-            <h2 className="popup-place__icon-close popup-place__icon-close:hover">+</h2>
-            <form name="add_place" className="popup-place__container form-popup" noValidate>
-              <h2 className="popup-place__text" >Nuevo Lugar</h2>
-              <input 
-                type="text"
-                id="titulo"
-                placeholder="Titulo"
-                minLength="2" maxLength="30"
-                value=""
-                className="popup-place__imput-text popup-place__imput-text_title form-imput-text"
-                required
-                autoComplete="off"
-              />
-              <span className="titulo-error form-input-show-error"></span>
-              <input type="url"
-                id="url"
-                placeholder="Imagen URL"
-                minLength="2" maxLength="200"
-                value=""
-                className="popup-place__imput-text popup-place__imput-text_image form-imput-text"
-                required
-                autoComplete="off"
-              />
-              <span className="url-error form-input-show-error"></span>
-              <button className="popup-save popup-place__button-save popup-place__button-save:hover">Guardar</button>
-            </form>
-          </div>
-        </div>
+        <PopupWithForm name="-place" id="popup-place_container" title="Nuevo Lugar">
+          <input 
+            type="text"
+            id="titulo"
+            placeholder="Titulo"
+            minLength="2" maxLength="30"
+            value=""
+            className="popup-place__imput-text popup-place__imput-text_title form-imput-text"
+            required
+            autoComplete="off"
+          />
+          <span className="titulo-error form-input-show-error"></span>
+          <input type="url"
+            id="url"
+            placeholder="Imagen URL"
+            minLength="2" maxLength="200"
+            value=""
+            className="popup-place__imput-text popup-place__imput-text_image form-imput-text"
+            required
+            autoComplete="off"
+          />
+          <span className="url-error form-input-show-error"></span>
+          <button className="popup-save popup-place__button-save popup-place__button-save:hover">Guardar</button>
+        </PopupWithForm>
+
       </div>
 
     </div>
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function PopupWithForm(props) {
+    return (
+        <>
+            <div className={`popup${props.name} popup-opened form`} id={props.id}>
+                <div className={`popup${props.name}__group`}>
+                    <h2 className={`popup${props.name}__icon-close popup${props.name}__icon-close:hover`}>+</h2>
+                    <form name={props.name} className={`popup${props.name}__container form-popup`} noValidate>
+                        <h2 className={`popup${props.name}__text`} >{props.title}</h2>
+                        {props.children}
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PopupWithForm
