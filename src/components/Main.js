777918591410b73706931f190ce39c537ae7ecc5
Kelvin Suarez <k_suarez22@hotmail.com>
2024-05-13 07:13:52 -0700

    tomar datos del perfil desde api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import VectorLapiz  from '../images/profile/lapiz.svg';
-import VectorCruz  from '../images/profile/button-add.svg'
+import VectorCruz  from '../images/profile/button-add.svg';
+import {api} from '../utils/api.js'
 
 function Main(props) {
+
+    const [userName, setUserName] = React.useState();
+    const [userDescription, setUserDescription] = React.useState();
+    const [userAvatar, setUserAvatar] = React.useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const userData = await api.getUserInfoFronServer();
+                setUserName(userData.name);
+                setUserDescription(userData.about);
+                setUserAvatar(userData.avatar);
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
             <section className="profile">
@@ -11,16 +31,16 @@ function Main(props) {
                         <div className="profile__image-edit">
                             <img className="profile__edit-image-button-vector" src={VectorLapiz} onClick={props.onEditAvatarClick}/>
                         </div>
-                        <img className="profile__image"  alt="foto de perfil foto de perfil foto de perfil"/>
+                        <img className="profile__image"  style={{ backgroundImage: `url(${userAvatar})` }}/>
                     </div>
                 </div>
                 <div className="profile__into">
-                    <h2 className="profile__title">Jacques Cousteau</h2>
+                    <h2 className="profile__title">{userName}</h2>
                     <div className="profile__edit-button-square profile__edit-button-square_grey" onClick={props.onEditProfileClick}> </div>
                     <div className="profile__edit-button-square" id="edit-button">
                         <img className="profile__edit-button-vector" src={VectorLapiz} alt="imagen de silueta de lapiz"/>
                     </div>
-                    <h3 className="profile__subtitle">Explorador</h3> 
+                    <h3 className="profile__subtitle">{userDescription}</h3> 
                 </div>
                 <div className="profile__add-button profile__add-button_grey" onClick={props.onAddPlaceClick}> </div>
                 <div className="profile__add-button profile__add-button:hover">
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,143 @@
-const api = new Api({
-    address: 'https://nomoreparties.co',
+import React from 'react';
+
+class Api{
+    constructor({token, address, groupId }){
+        this._authorization = token;
+        this._address = address;
+        this._groupId = groupId
+    }
+
+    async _useFetch(url, method, body){
+        const res = await fetch(url, {
+            headers: {
+                authorization: this._authorization,
+                "content-type": "application/json"
+            },
+            method,
+            body: JSON.stringify(body)
+        });
+
+        if (res.ok){
+            return res.json();
+        }
+
+        return Promise.reject(`Error ${res.status}`)
+    }
+
+    async getUserInfoFronServer() {
+        try{
+            const res = await this._useFetch(`${this._address}/v1/${this._groupId}/users/me`,
+            "GET"
+            );
+            return res;
+        }catch(err) {
+            console.log(err);
+        }
+    }
+
+    async getCards(){
+        try{
+            const res = await this._useFetch(`${this._address}/v1/${this._groupId}/cards`,
+            "GET"
+            );
+            return res;
+        }catch (err) {
+            console.log(err);
+        }
+    }
+
+    async saveDataToServer(name, job) {
+        try{
+            const res = await this._useFetch(
+                `${this._address}/v1/${this._groupId}/users/me`,
+                "PATCH",
+                {
+                    name,
+                    about: job,
+                }
+            );
+            return res;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async addNewCardToServer(name, link) {
+        try {
+            const res = await this._useFetch(
+               `${this._address}/v1/${this._groupId}/cards`,
+               "POST",
+               {
+                name: name,
+                link: link,
+               }
+            );
+            return res;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    async showLikeFromCard(cardId) {
+        try {
+            const res = await this._useFetch(
+                `${this._address}/v1/${this._groupId}/cards/likes/${cardId}`,
+                "PUT"
+            );
+
+            return res;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async deleteLikeFromCard(cardId){
+        try {
+            const res = await this._useFetch(
+                `${this._address}/v1/${this._groupId}/cards/likes/${cardId}`,
+                "DELETE"
+            );
+
+            return res;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async deleteCardFromServer(cardId){
+        try {
+            const res = await this._useFetch(
+                `${this._address}/v1/${this._groupId}/cards/${cardId}`,
+                "DELETE"
+            );
+
+            return res;
+        } catch (err) {
+            console.log (err);
+        }
+    }
+
+    async updateImageProfile(avatarUrl) {
+        try {
+            const res = await this._useFetch(
+                `${this._address}/v1/${this._groupId}/users/me/avatar`,
+                "PATCH",
+                {
+                    avatar: avatarUrl,
+                }
+            );
+
+            return res;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+}
+
+
+export const api = new Api({
+    address: 'https://around.nomoreparties.co',
     groupId: `web_es_11`,
     token: `ddd525c3-4e22-45dd-b59e-e1d482e580b4`
 });
